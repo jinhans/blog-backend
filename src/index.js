62694a002d89f2456b8d04ff5b0097da17d444d0
jinhans <wlsgks846@gmail.com>
2023-01-12 15:04:15 +0900

    기본 사용법

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401; //Unauthorized
-    return;
-  }
-  await next();
-  console.log('END');
+router.get('/', (ctx) => {
+  ctx.body = '홈';
+});
+router.get('/about', (ctx) => {
+  ctx.body = '소개';
 });
 
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
-
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
