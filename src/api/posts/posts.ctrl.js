b286f2e7ec0bf4a27cb46ad19fe00675fcec7aa4
jinhans <wlsgks846@gmail.com>
2023-01-18 00:04:04 +0900

    블로그 포스트 작성하는 api인 write

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,6 +1,19 @@
 import Post from '../../models/post';
 
-export const write = (ctx) => {};
+export const write = async (ctx) => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 export const list = (ctx) => {};
 
