bcd7b9fbac22146245744234c38f4d989fd3db26
jinhans <wlsgks846@gmail.com>
2023-01-19 15:54:09 +0900

    페이지 기능 구현

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -42,8 +42,19 @@ export const write = async (ctx) => {
 };
 
 export const list = async (ctx) => {
+  const page = parseInt(ctx.query.page || '1', 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   try {
-    const posts = await Post.find().sort({ _id: -1 }).limit(10).exec();
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .exec();
     ctx.body = posts;
   } catch (e) {
     ctx.throw(500, e);
