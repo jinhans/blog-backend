a9aba5f7218356a1a153d9db99e0655fba71a1a0
jinhans <wlsgks846@gmail.com>
2023-01-23 15:56:17 +0900

    토큰 발급하기

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -30,6 +30,12 @@ export const register = async (ctx) => {
     await user.save(); // 데이터베이스에 저장
 
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -56,6 +62,11 @@ export const login = async (ctx) => {
       return;
     }
     ctx.body = user.serialize();
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
